a34e96f45b625145aa459addc381a440fb359c4d
Khoaoe <khoaoe@gmail.com>
2024-06-21 22:39:04 +0700

    update code for week 03  module 01

---
+++ module_01/week_03/TN/ex9.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from TL.ex3 import Stack as MyStack
+
+
+stack1 = MyStack(capacity=5)
+stack1.push(1)
+assert stack1.is_full() == False
+stack1.push(2)
+print(stack1.is_full())
