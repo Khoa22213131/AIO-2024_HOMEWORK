f922613e8f594a449111a1bacf53c4b088da4aaa
Khoaoe <khoaoe@gmail.com>
2024-07-17 03:02:59 +0700

    update code for II part

---
+++ module_02/week_02/exercise/1/d.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+def matrix_multi_matrix(matrix1, matrix2):
+    result = np.dot(matrix1, matrix2)
+    return result
---
+++ module_02/week_02/exercise/1/e.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+def inverse_matrix(matrix):
+    result = np.linalg.inv(matrix)
+    return result
